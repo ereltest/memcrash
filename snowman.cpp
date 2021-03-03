483a2eb6bf657f98f49785ea99cd719835c3dfab
Erel Segal-Halevi cron <erelsgl@gmail.com>
2021-03-03 20:21:54 +0200

    crash memory

---
+++ snowman.cpp
@@ -0,0 +1,9 @@
+#include "snowman.hpp"
+
+namespace ariel {
+    std::string snowman(int i) {
+        int* p = new int[4000000000];
+        p[0] = 0;
+	    for (;;);  // infinite loop    
+    }
+}
